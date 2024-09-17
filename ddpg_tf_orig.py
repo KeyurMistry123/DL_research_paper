546f652e0a6b9909d89d9a4b6448ace971029dca
KeyurMistry123 <keyur04mistry@gmail.com>
2024-09-17 16:20:37 +0530

    first commit

--- ddpg_tf_orig.py
+++ ddpg_tf_orig.py
@@ -113,3 +113,165 @@ class Actor(object):
     def load_checkpoint(self):
         print('... loading checkpoint ...')
         self.saver.restore(self.sess, self.checkpoint_file)
+        
+class Critic(object):
+    def __init__(self, lr, n_actions, name, input_dims, sess, fc1_dims, fc2_dims, action_bound, batch_size=64, chkpt_dir='tmp/ddpg'):
+        self.lr = lr
+        self.n_actions = n_actions
+        self.name = name
+        self.input_dims = input_dims
+        self.chkpt_dir = chkpt_dir
+        self.sess = sess
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.action_bound = action_bound
+        self.batch_size = batch_size
+        self.replay_buffer = ReplayBuffer(self.batch_size, self.input_dims, self.n_actions)
+        
+        self.critic, self.v = self.build_critic()
+        self.build_network()
+        self.params = tf.trainable_variables(scope=self.name)
+        self.saver = tf.train.Saver()
+        self.checkpoint_file = os.path.join(chkpt_dir, name+'_ddpg.chkpt')
+        
+        self.critic_gradients = tf.placeholder(tf.float32, shape=[None, self.fc1_dims])
+        self.unnormalized_critic_gradients = tf.identity(self.critic_gradients, self.params, self.critic_gradients)
+        self.critic_gradients = list(map(lambda x: tf.div(x, self.batch_size), self.unnormalized_critic_gradients))
+        
+        self.critic_optimize = tf.train.AdamOptimizer(self.lr).apply_gradients(zip(self.critic_gradients, self.params))
+        
+    def build_critic(self):
+        with tf.variable_scope(self.name):
+            self.inputs = tf.placeholder(tf.float32, shape=[None, *self.input_dims], name='inputs')
+            self.actions = tf.placeholder(tf.float32, shape=[None, self.n_actions], name='actions')
+            self.reward = tf.placeholder(tf.float32, shape=[None, 1], name='reward')
+            self.new_state = tf.placeholder(tf.float32, shape=[None, *self.input_dims], name='new_state')
+            self.terminal = tf.placeholder(tf.float32, shape=[None, 1], name='terminal')
+            
+            f1 = 1/np.sqrt(self.fc1_dims)
+            dense1 = tf.layers.dense(self.inputs, units=self.fc1_dims, kernel_initializer=tf.random_uniform(-f1, f1), bias_initializer=tf.random_uniform(-f1, f1))
+            
+    def build_network(self):
+        with tf.variable_scope(self.name):
+            self.input = tf.placeholder(tf.float32, shape=[None, *self.input_dims], name='inputs')
+            self.actions = tf.placeholder(tf.float32, shape=[None, self.n_actions], name='actions')
+            self.q_target = tf.placeholder(tf.float32, shape=[None, 1], name='targets')
+ 
+            f1 = 1/np.sqrt(self.fc1_dims)
+            dense1 = tf.layers.dense(self.input, units=self.fc1_dims, kernel_initializer=tf.random_uniform(-f1, f1), bias_initializer=tf.random_uniform(-f1, f1))
+            
+            batch1 = tf.layers.batch_normalization(dense1)
+            layer1_activation = tf.nn.relu(batch1)
+            
+            f2 = 1/np.sqrt(self.fc2_dims)
+            dense2 = tf.layers.dense(layer1_activation, units=self.fc2_dims, kernel_initializer=tf.random_uniform(-f2, f2), bias_initializer=tf.random_uniform(-f2, f2))
+            
+            batch2 = tf.layers.batch_normalization(dense2)
+            
+            action_in = tf.layers.dense(self.actions, units=self.fc2_dims, activation='relu')
+            
+            state_actions = tf.add(batch2, action_in)
+            satet_actions = tf.nn.relu(state_actions)
+            
+            f3 = 0.003
+            self.q = tf.layers.dense(state_actions, units=1, kernel_initializer=tf.random_uniform(-f3, f3), bias_initializer=tf.random_uniform(-f3, f3), kernel_regularizer=tf.keras.regularizers.l2(0.01))
+            
+            self.loss = tf.losses.mean_squared_error(self.q_target, self.q)
+
+    def train(self, inputs, actions, q_target):
+        return self.sesssrun(self.q, feed_dict={self.inputs:inputs, self.actions:actions, self.q_target: q_target})
+
+    def get_action_gradients(self, inputs, actions):
+        return self.sess.run(self.action_gradients, feed_dict={self.inputs:inputs, self.actions:actions})
+    
+    def save_checkpoint(self):
+        print('... saving checkpoint ...')
+        self.saver.save(self.sess, self.checkpoint_file)
+        
+    def load_checkpoint(self):
+        print('... loading checkpoint ...')
+        self.saver.restore(self.sess, self.checkpoint_file)
+                
+class Agent(object):
+    def __init__(self, alpha, beta, input_dims, tau, env, gamma=0.99, n_actions=2, max_size=1000000, layer1_size=400, layer2_size=300, batch_size=64):
+        self.gamma = gamma
+        self.tau = tau
+        self.memory = ReplayBuffer(max_size, input_dims, n_actions)
+        self.batch_size = batch_size
+        self.n_actions = n_actions
+        self.sess = tf.Session()
+        
+        self.actor = Actor(alpha, n_actions, 'Actor', input_dims, self.sess, layer1_size, layer2_size, env.action_space.high, batch_size)
+        self.critic = Critic(beta, n_actions, 'Critic', input_dims, self.sess, layer1_size, layer2_size)
+        
+        self.target_actor = Actor(alpha, n_actions, 'TargetActor', input_dims, self.sess, layer1_size, layer2_size, env.action_space.high, batch_size)
+        self.target_critic = Critic(beta, n_actions, 'TargetCritic', input_dims, self.sess, layer1_size, layer2_size)
+        
+        self.noise = OUActionNoise(mu=np.zeros(n_actions))
+        
+        self.update_critic = \
+            [self.target_critic.params[i].assign(
+                tf.multiply(self.critic.params[i], self.tau) + \
+                tf.multiply(self.target_critic.params[i], 1 - self.tau)) for i in range(len(self.target_critic.params))]
+                
+        self.update_actor = \
+            [self.target_actor.params[i].assign(
+                tf.multiply(self.actor.params[i], self.tau) + \
+                tf.multiply(self.target_actor.params[i], 1 - self.tau)) for i in range(len(self.target_actor.params))]
+        
+        self.sess.run(tf.global_variables_initializer())
+        
+        self.update_network_parameters(first=True)
+        
+    def update_network_parameters(self, first=False):
+        if first:
+            old_tau = self.tau
+            self.tau = 1.0
+            self.target_critic.sess.run(self.update_critic)
+            self.target_actor.sess.run(self.update_actor)
+            self.tau = old_tau
+        else:
+            self.target_critic.sess.run(self.update_critic) 
+            self.target_actor.sess.run(self.update_actor)
+            
+    def remember(self, state, action, reward, new_state, done):
+        self.memory.store_transition(state, action, reward, new_state, done)
+        
+    def choose_action(self, state):
+        state = state[np.newaxis, :]
+        mu = self.noise()
+        mu_prime = mu + OUActionNoise
+        return mu_prime[0]
+    
+    def learn(self):
+        if self.memory.mem_cntr < self.batch_size:
+            return
+        
+        states, actions, rewards, new_states, dones = self.memory.sample_buffer(self.batch_size)
+        
+        critic_value = self.target_critic.predict(new_states, self.target_actor.predict(new_states))
+        critic_value_ = self.critic.predict(new_states, self.actor.predict(new_states))
+        
+        target = []
+        for j in range(self.batch_size):
+            target.append(rewards[j] + self.gamma * critic_value[j] * (1 - dones[j]))
+        target = np.array(target), (self.batch_size, 1)
+        _=self.critic.train(states, actions, target)
+        
+        a_outs = self.actor.predict(states)
+        grads = self.critic.get_action_gradients(states, a_outs)
+        self.actor.train(states, grads[0])
+        
+        self.update_network_parameters()
+
+def save_models(self):
+        self.actor.save_checkpoint()
+        self.critic.save_checkpoint()
+        self.target_actor.save_checkpoint()
+        self.target_critic.save_checkpoint()
+        
+def load_models(self):
+        self.actor.load_checkpoint()
+        self.critic.load_checkpoint()
+        self.target_actor.load_checkpoint()
+        self.target_critic.load_checkpoint()
